4051a5f7235addf54dc56dcf9323a45188c4f5ab
SamaTech <samatechtw@gmail.com>
2021-10-01 00:30:20 +0800

    add release workflow and update types

--- index.d.ts
+++ index.d.ts
@@ -1,37 +1,37 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-declare module 'fcl' {
-  type AnyJson = boolean | number | string | null | IJsonArray | IJsonObject
+declare module 'vite-flow-fcl' {
+  type AnyJson = boolean | number | string | null | IJsonArray | IJsonObject;
 
   interface IJsonObject {
-    [key: string]: AnyJson
+    [key: string]: AnyJson;
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface IJsonArray extends Array<AnyJson> {}
 
-  type Decoder = (dictionary, decoders, stack) => Record<any, any>
-  type DecoderGroup = Record<string, Decoder>
-  type Response = IJsonObject
+  type Decoder = (dictionary, decoders, stack) => Record<any, any>;
+  type DecoderGroup = Record<string, Decoder>;
+  type Response = IJsonObject;
 
   interface BlockObject {
-    id: string
-    parentId: string
-    height: number
-    timestamp: any
-    collectionGuarantees: any
-    blockSeals: any
-    signatures: Uint8Array
+    id: string;
+    parentId: string;
+    height: number;
+    timestamp: any;
+    collectionGuarantees: any;
+    blockSeals: any;
+    signatures: Uint8Array;
   }
 
-  function send(args: any, opts?: any): Promise<Response>
+  function send(args: any, opts?: any): Promise<Response>;
 
   function decode(
     decodeInstructions: any,
     customDecoders?: DecoderGroup,
     stack?: Array<any>,
-  ): Promise<any>
+  ): Promise<any>;
 
-  function latestBlock(isSealed: boolean): Promise<BlockObject>
+  function latestBlock(isSealed: boolean): Promise<BlockObject>;
 
-  export { decode, send, latestBlock }
+  export { decode, send, latestBlock };
 }
